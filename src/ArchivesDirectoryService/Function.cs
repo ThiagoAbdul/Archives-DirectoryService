8a77bdc80b677e3ae74adab25702add227e2d414
Thiago Guedes <thiago.guedes@evosystems.com.br>
2026-01-06 22:48:20 -0300

    first commit

---
+++ src/ArchivesDirectoryService/Function.cs
@@ -0,0 +1,34 @@
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ArchivesDirectoryService;
+
+public class Function
+{
+    private static async Task Main()
+    {
+        Func<APIGatewayHttpApiV2ProxyRequest, ILambdaContext, APIGatewayHttpApiV2ProxyResponse> handler = FunctionHandler;
+        await LambdaBootstrapBuilder.Create(handler, new SourceGeneratorLambdaJsonSerializer<LambdaFunctionJsonSerializerContext>())
+            .Build()
+            .RunAsync();
+    }
+
+    public static APIGatewayHttpApiV2ProxyResponse FunctionHandler(APIGatewayHttpApiV2ProxyRequest input, ILambdaContext context)
+    {
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            StatusCode = 200,
+            Body = JsonSerializer.Serialize(new Response("World"), LambdaFunctionJsonSerializerContext.Default.Response)
+        };
+    }
+}
+
+public record Response(string Hello);
+
+[JsonSerializable(typeof(APIGatewayHttpApiV2ProxyRequest))]
+[JsonSerializable(typeof(Response))]
+public partial class LambdaFunctionJsonSerializerContext : JsonSerializerContext {}
