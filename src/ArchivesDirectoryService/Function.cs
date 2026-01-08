c3c221ddda6a4574801b5f577230e5aa036395f7
ThiagoAbdul <thiagoabdul10@gmail.com>
2026-01-08 01:15:23 -0300

    trace: debug

--- src/ArchivesDirectoryService/Function.cs
+++ src/ArchivesDirectoryService/Function.cs
@@ -35,23 +35,25 @@ public class Function
         switch(input.RequestContext.Http.Method)
         {
             case "GET":
-                return await GetArchivesAsync(input, userId!);
+                return await GetArchivesAsync(input, userId!, context);
             case "POST":
-                return await CreateFolderAsync(input, userId);
+                return await CreateFolderAsync(input, userId, context);
 
             default:
                 return Response(405);
         }
     }
 
-    private static async Task<APIGatewayHttpApiV2ProxyResponse> GetArchivesAsync(APIGatewayHttpApiV2ProxyRequest input, string userId)
+    private static async Task<APIGatewayHttpApiV2ProxyResponse> GetArchivesAsync(APIGatewayHttpApiV2ProxyRequest input, string userId, ILambdaContext context)
     {
         string? parentId = null;
         bool hasFolder = input.QueryStringParameters?.TryGetValue("parent", out parentId) ?? false;
 
+        context.Logger.LogInformation("antes de buscar no DynamoDB");
 
         var archives = await repository.GetArchivesAsync(userId!, parentId);
 
+        context.Logger.LogInformation("após buscar no DynamoDB");
 
         var content = archives.Select(a => new ArchiveResponse(a));
 
@@ -62,11 +64,14 @@ public class Function
 
         var body = JsonSerializer.Serialize(content, options)!;
 
+        context.Logger.LogInformation("Antes do response");
+
+
         return Response(200, body);
 
     }
 
-    private static async Task<APIGatewayHttpApiV2ProxyResponse> CreateFolderAsync(APIGatewayHttpApiV2ProxyRequest input, string userId)
+    private static async Task<APIGatewayHttpApiV2ProxyResponse> CreateFolderAsync(APIGatewayHttpApiV2ProxyRequest input, string userId, ILambdaContext context)
     {
         var command = JsonSerializer.Deserialize(input.Body, LambdaFunctionJsonSerializerContext.Default.CreateFolderCommand);
 
